7f9d93e769395bd385bcdae846cb03cdea3e2ca3
Zayus13 <sven_loechte@protonmail.com>
2024-09-08 16:56:33 +0200

    new test params in main

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from gui.pyglet_gui import CarsWindow
 from scenarios import LEFT_RIGHT_OVERTAKE, STARTING_SCENARIO, ONE_ROAD, HORIZONTAL_VERTICAL
 
 
-def main(players, roads, segmentation, debug=False):
+def main(players, roads, segmentation, debug=False, test=False, test_mode=None):
     game = TrafficEnv(players=players, roads=roads)
     controllers = [AstarCarController(game=game, player=i) for i in range(players)]
 
-    CarsWindow(game, controllers, segmentation=segmentation, debug=debug)
+    CarsWindow(game, controllers, segmentation=segmentation, debug=debug, test=test, test_mode=test_mode)
 
 
 if __name__ == '__main__':
-    main(**STARTING_SCENARIO, debug=True)
+    main(**STARTING_SCENARIO, debug=False, test=True, test_mode=["all"])
