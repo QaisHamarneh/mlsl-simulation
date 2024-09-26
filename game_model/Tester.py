e2cb41df8280e93d6b0d50905de7866458156d0b
Zayus13 <sven_loechte@protonmail.com>
2024-09-26 16:58:37 +0200

    doc adjusted

--- game_model/Tester.py
+++ game_model/Tester.py
@@ -1,5 +1,5 @@
 from math import gamma
-from typing import List
+from typing import List, Tuple, Optional
 
 from controller.astar_car_controller import AstarCarController
 from game_model.game_model import TrafficEnv
@@ -32,15 +32,15 @@ class SimulationTester:
         self.debug_mode = debug_mode if "all" not in debug_mode else self.modes.keys()
         self.rate = rate
 
-    def run(self) -> List[bool]:
+    def run(self) -> Optional[List[Tuple[bool, str]]]:
         """
         Run the selected debug modes every rate time steps.
 
         Returns:
-            List[bool]: The results of the debug modes.
+            Optional[List[Tuple[bool, str]]]: The results of the debug modes, None if tests were not conducted on that frame.
         """
         if self.controllers[0].car.time % self.rate != 0:
-            return
+            return None
 
         results = []
 
@@ -49,7 +49,7 @@ class SimulationTester:
 
         return results
 
-    def reserved_check(self) -> bool:
+    def reserved_check(self) -> Tuple[bool, str]:
         """
         Checks if the last segment of each car is a lane segment.
         Prints the number of cars that are not in a lane segment.
@@ -72,7 +72,7 @@ class SimulationTester:
 
         return not not_in_lane, res_string
 
-    def priority_check(self) -> bool:
+    def priority_check(self) -> Tuple[bool, str]:
         """
         Check if the priority of cars is correct:
         If the current segment is a crossing segment, the priority should be 0.
@@ -102,7 +102,7 @@ class SimulationTester:
 
         return not incorrect_priority, res_string
 
-    def reservation_check(self):
+    def reservation_check(self) -> Tuple[bool, str]:
         """
         Check if the car's reserved space is at least as long as its breaking distance.
         If it is longer, the reserved segment must include a path through a crossing,
@@ -129,15 +129,15 @@ class SimulationTester:
                     no_correct_reservation_on_last_segment += 1
 
         if too_short_reservations > 0 or no_correct_reservation_on_last_segment > 0:
-            res_string = (f"Reservation Check: {too_short_reservations + no_correct_reservation_on_last_segment} cars have incorrect reservations, "
-                          f"{too_short_reservations} are too short, {no_correct_reservation_on_last_segment} have no correct reservation on the last segment")
+            res_string = (
+                f"Reservation Check: {too_short_reservations + no_correct_reservation_on_last_segment} cars have incorrect reservations, "
+                f"{too_short_reservations} are too short, {no_correct_reservation_on_last_segment} have no correct reservation on the last segment")
         else:
             res_string = "Reservation Check: Each car has correct reservations"
 
         return not (too_short_reservations + no_correct_reservation_on_last_segment), res_string
 
-
-    def consistency_check(self):
+    def consistency_check(self) -> Tuple[bool, str]:
         """
         Check if the cars' reserved spaces are consistent with the cars in the segments.
         The function prints the number of cars with inconsistent reservations.
@@ -161,8 +161,9 @@ class SimulationTester:
                     additional_reservations += 1
 
         if missing_reservations > 0 or additional_reservations > 0:
-            res_string = (f"Consistency Check: {missing_reservations + additional_reservations} inconsistent reservations, "
-                          f"{missing_reservations} missing reservations, {additional_reservations} additional reservations")
+            res_string = (
+                f"Consistency Check: {missing_reservations + additional_reservations} inconsistent reservations, "
+                f"{missing_reservations} missing reservations, {additional_reservations} additional reservations")
         else:
             res_string = "Consistency Check: Each car has consistent reservations"
 
